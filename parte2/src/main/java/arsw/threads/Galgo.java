612e1376f0e963195924e952957f429689a665ed
Alejandro Prieto <samuel.prieto-r@mail.escuelaing.edu.co>
2025-08-25 19:39:50 -0500

    Agregando hasta parte 2.4

---
+++ parte2/src/main/java/arsw/threads/Galgo.java
@@ -0,0 +1,58 @@
+package arsw.threads;
+
+/**
+ * Un galgo que puede correr en un carril
+ * 
+ * @author rlopez
+ * 
+ */
+public class Galgo extends Thread {
+	private int paso;
+	private Carril carril;
+	RegistroLlegada regl;
+
+	public Galgo(Carril carril, String name, RegistroLlegada reg) {
+		super(name);
+		this.carril = carril;
+		paso = 0;
+		this.regl = reg;
+	}
+
+	public void corra() throws InterruptedException {
+		while (paso < carril.size()) {
+			Thread.sleep(100);
+			carril.setPasoOn(paso++);
+			carril.displayPasos(paso);
+
+			if (paso == carril.size()) {
+    carril.finish();
+
+    synchronized(regl) {
+        // incrementar la posición de llegada de forma segura
+        int ubicacion = regl.getUltimaPosicionAlcanzada();
+        regl.setUltimaPosicionAlcanzada(ubicacion + 1);
+
+        // solo el primero será ganador
+        if (ubicacion == 1) {
+            regl.setGanador(this.getName());
+        }
+
+        System.out.println("El galgo " + this.getName() + " llegó en la posición " + ubicacion);
+    }
+}
+
+		}
+	}
+
+	@Override
+	public void run() {
+
+		try {
+			corra();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
